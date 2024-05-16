0d9123f49a34ae6010979007c8a9e01e5652124e
Sujal <morwanisujal875@gmail.com>
2024-05-16 22:03:03 +0530

    simple react proj

--- simpleproject/src/App.jsx
+++ simpleproject/src/App.jsx
@@ -1,29 +1,42 @@
-import React from 'react'
+// import React from 'react'
 
+// const App = () => {
+//   const name ='Sujal';
+//   const x=23;
+//   const y=20;
+//   const names =['Brad','Hog','Fran']
+//   const loggedIn =false;
+//   const styles ={
+//     color: 'red',
+//     fontSize: '20px',
+// };
+//   return (
+//     <div>
+//     <div className='text-5xl'>
+//       App
+//     </div>
+//     <p style={styles}>Hello {name}</p>
+//     <p>The value of x is{x+y}</p>
+//     <ul>
+//       {names.map((name,index) => (
+//         <li key={index}>{name}</li>
+//       ))}
+//     </ul>
+//     {loggedIn ? <h1>Hello Member</h1> : <h1>Hello Guest</h1>}
+//       </div>
+//   )
+// }
+
+// export default App
+import React from 'react'
+import Navbar from './components/Navbar'
 const App = () => {
-  const name ='Sujal';
-  const x=23;
-  const y=20;
-  const names =['Brad','Hog','Fran']
-  const loggedIn =false;
-  const styles ={
-    color: 'red',
-    fontSize: '20px',
-};
   return (
     <div>
-    <div className='text-5xl'>
-      App
-    </div>
-    <p style={styles}>Hello {name}</p>
-    <p>The value of x is{x+y}</p>
-    <ul>
-      {names.map((name,index) => (
-        <li key={index}>{name}</li>
-      ))}
-    </ul>
-    {loggedIn ? <h1>Hello Member</h1> : <h1>Hello Guest</h1>}
+      <div>
+      <Navbar title="Finding the Job" subtitle="The requirements of the job"/>
       </div>
+    </div>
   )
 }
 
